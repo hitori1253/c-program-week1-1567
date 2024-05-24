f66143638cf4761f45c82878155dbd35f6ea3f3d
hitori <jont2514@gmail.com>
2024-05-24 16:01:07 +0700

    first commit

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main (){
+    //Create integer variable
+    int age = 15;
+    //Print Hello World to the screen
+    printf("Hello World\n");
+    printf("My Name is Hitori Pik!\n");
+    printf("Age: %d\n",age);
+    return 0;
+}
